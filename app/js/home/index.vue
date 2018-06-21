f1eb3299e8a98bca6597e6cc786b5545da6719e8
showa11 <shibushishang@gmail.com>
2018-06-21 18:23:11 +0800

    home component finish

--- app/js/home/index.vue
+++ app/js/home/index.vue
@@ -6,24 +6,30 @@
         <borrow/>
         <money/>
         <product/>
+        <life/>
+        <home-footer/>
     </div>
 </template>
 
 <script>
 import HomeHeader from "../public/header.vue"
+import HomeFooter from "../public/footer.vue"
 import HSlider from "./HSlider.vue"
 import novice from "./novice.vue"
 import Borrow from "./Borrow.vue"
 import Money from "./Money.vue"
 import Product from "./Product.vue"
+import Life from "./life.vue"
 export default {
     components: {
         HomeHeader,
+        HomeFooter,
         HSlider,
         novice,
         Borrow,
         Money,
         Product,
+        Life,
     },
 }
 </script>
